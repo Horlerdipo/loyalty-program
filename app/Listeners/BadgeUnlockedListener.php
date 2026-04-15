d5dc8125129739c778f17669e30f8a7984d4cb8e
Umar Oladipo <oladipoumar@gmail.com>
2026-04-15 09:36:17 +0100

    Events and Listeners set up

---
+++ app/Listeners/BadgeUnlockedListener.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\BadgeUnlocked;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+
+class BadgeUnlockedListener implements ShouldQueue
+{
+    use InteractsWithQueue;
+
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(BadgeUnlocked $event): void
+    {
+        //
+    }
+}
